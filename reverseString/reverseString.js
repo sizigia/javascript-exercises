b67ac4e04657a97aac85771bb0857a9136489ac0
sizigia <cumbresdigitales@gmail.com>
2019-02-26 10:10:27 -0300

    solution to reverse string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function() {
+    let inputString = 'hello';
+    let inputArray = inputString.split('');
+    let revInput = inputArray.reverse().join('').toString();
 
+        return revInput;
 }
 
 module.exports = reverseString
