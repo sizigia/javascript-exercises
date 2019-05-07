3f10b841528ee5928d6b8a9af8f866480dc64feb
sizigia <cumbresdigitales@gmail.com>
2019-05-07 13:22:32 -0300

    solution to palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
-
+const palindromes = function (str) {
+    myString = str.toLowerCase().split("").filter(x => x.match(/[a-zA-z]+/g)).join("");
+    reverseString = myString.split("").reverse().join("");
+    return reverseString == myString;
 }
 
 module.exports = palindromes
