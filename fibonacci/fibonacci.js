1a7c678cbce0e3e49602d9952ad78436945539f0
sizigia <cumbresdigitales@gmail.com>
2019-05-07 19:56:31 -0300

    solution to fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (element) {
+    let n = parseInt(element, 10) - 1;
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    let fibo = {
+        one: 0,
+        two: 1,
+    }
+
+    while (n > 1, n--) {
+        fibo.sum = fibo.one + fibo.two;
+        fibo.one = fibo.two;
+        fibo.two = fibo.sum;
+    }
+
+    return fibo.two;
 }
 
 module.exports = fibonacci
